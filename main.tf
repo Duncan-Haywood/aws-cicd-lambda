80dffaf20ddd3cb3253dc511f2addab5ab870e94
Duncan Haywood <duncan.haywood.ai@gmail.com>
2023-08-09 10:38:55 -0600

    add api gateway

--- main.tf
+++ main.tf
@@ -42,3 +42,54 @@ resource "aws_iam_role" "lambda" {
 }
 EOF
 }
+
+# API Gateway REST API
+resource "aws_api_gateway_rest_api" "nginx" {
+  name = "nginx"
+}
+
+# API Gateway resource
+resource "aws_api_gateway_resource" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.nginx.id
+  parent_id   = aws_api_gateway_rest_api.nginx.root_resource_id
+  path_part   = "{proxy+}"
+}
+
+# API Gateway method
+resource "aws_api_gateway_method" "proxy" {
+  rest_api_id   = aws_api_gateway_rest_api.nginx.id
+  resource_id   = aws_api_gateway_resource.proxy.id
+  http_method   = "ANY"
+  authorization = "NONE" 
+}
+
+# Integrate method with Lambda
+resource "aws_api_gateway_integration" "lambda" {
+  rest_api_id = aws_api_gateway_rest_api.nginx.id
+  resource_id = aws_api_gateway_method.proxy.resource_id
+  http_method = aws_api_gateway_method.proxy.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.nginx.invoke_arn
+}
+
+# Deploy API
+resource "aws_api_gateway_deployment" "nginx" {
+  depends_on = [
+    aws_api_gateway_integration.lambda,
+  ]
+
+  rest_api_id = aws_api_gateway_rest_api.nginx.id
+  stage_name  = "test"
+}
+
+# Create usage plan
+resource "aws_api_gateway_usage_plan" "nginx" {
+  name = "nginx"
+
+  api_stages {
+    api_id = aws_api_gateway_rest_api.nginx.id
+    stage  = aws_api_gateway_deployment.nginx.stage_name
+  }
+}
